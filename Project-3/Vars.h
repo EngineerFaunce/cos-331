d3b254114063a95217d442cf2d0e84576f6e6da3
William Faunce <3881480-EngineerFaunce@users.noreply.gitlab.com>
2019-12-14 08:41:07 -0500

    Change tab size to 4 spaces

--- Project-3/Vars.h
+++ Project-3/Vars.h
@@ -11,16 +11,16 @@
 
 struct PCB
 {
-     struct PCB *Next_PCB;
-     int PID;                      // process ID (0,1,2,3, etc.)
-     short int PC, P0, P1, P2, P3; // program counter and pointer registers
-     int IC;                       // number of instructions before preemption (i.e., time slice)
-     int R0, R1, R2, R3;           // general registers
-     char PSW[2];                  // Process status word
-     int ACC;                      // accumulator
-     int BaseReg, LimitReg;        // minimum and maximum memory location
-     int Total_Pages;
-     /* Note: You cannot store the process' memory in PCB */
+    struct PCB *Next_PCB;
+    int PID;                      // process ID (0,1,2,3, etc.)
+    short int PC, P0, P1, P2, P3; // program counter and pointer registers
+    int IC;                       // number of instructions before preemption (i.e., time slice)
+    int R0, R1, R2, R3;           // general registers
+    char PSW[2];                  // Process status word
+    int ACC;                      // accumulator
+    int BaseReg, LimitReg;        // minimum and maximum memory location
+    int Total_Pages;
+    /* Note: You cannot store the process' memory in PCB */
 };
 
 /* Pointers to head + tail of ready queue as well as current process */
@@ -56,12 +56,12 @@ short int opcode;
 
 struct Program
 {
-     int Last;
-     int Num_Lines;
-     int Data_Pages;
-     int Total_Pages;
-     char temp_mem[100][6];
-     struct PCB *New_PCB;
+    int Last;
+    int Num_Lines;
+    int Data_Pages;
+    int Total_Pages;
+    char temp_mem[100][6];
+    struct PCB *New_PCB;
 };
 
 int Page_Table[10][5];
