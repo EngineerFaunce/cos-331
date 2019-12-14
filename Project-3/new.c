bb707d2d5ebc3b79c8df5eab67f57e4a539a7fce
William Faunce <3881480-EngineerFaunce@users.noreply.gitlab.com>
2019-12-13 23:04:16 -0500

    Add old functions to new.c

--- Project-3/new.c
+++ Project-3/new.c
@@ -7,8 +7,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <math.h>
 #include "Vars.h"
-#include "Functions.h"
+//#include "Functions.h"
 
 struct PCB *Admit_Program();
 void Place_On_Queue(struct PCB *);
@@ -18,22 +19,28 @@ void Free_Pages(struct PCB *Current);
 void Print_Frames();
 int Find_Frame();
 
+void RestoreState(struct PCB *NextProc);
+void SaveState(struct PCB **PrevProc);
+void DeletePCB(struct PCB *Current);
+struct PCB *GetNextProcess(struct PCB **Head);
+void MvToTail(struct PCB *Current, struct PCB **RQT);
+void PrintQ(struct PCB *Head);
+
 struct Program *New_Allocation;
 
-int NAF = 10;
+int NAF = 10;                                               // number of available frames
 int Free_Frames[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 int On_Disk[10] = {5, 3, 3, 5, 5, 3, 3, 3, 5, 5};           // simulated storage that contains programs of different sizes
 int disk_index = 0;                                         // index used to access On_Disk array
 int num_requested;                                          // number of pages requested
-//char temp_memory[21][6];                                    // temp memory (which doesn't seem to be used)
+char temp_memory[21][6];                                    // temp memory (which doesn't seem to be used)
 char input[7];                                              // used for reading input from file
-
 
 /* Global variables used in function implementations */
 int fp, i, j, k;
 
 /*
-Admits a program into memory with the following steps:
+    Admits a program into memory with the following steps:
     - Determines the next program to admit
     - Determines the number of pages the program requires
     - Determines if the program can be brought into memory
@@ -195,4 +202,104 @@ int Find_Frame()
         }
     }
     return (-1);
+}
+
+/* Old functions */
+// Restores the state of the process that is set to begin its execution
+void RestoreState(struct PCB *NextProc)
+{
+    PRegs[0] = NextProc->P0;
+    PRegs[1] = NextProc->P1;
+    PRegs[2] = NextProc->P2;
+    PRegs[3] = NextProc->P3;
+
+    RRegs[0] = NextProc->R0;
+    RRegs[1] = NextProc->R1;
+    RRegs[2] = NextProc->R2;
+    RRegs[3] = NextProc->R3;
+
+    BaseRegister = NextProc->BaseReg;
+    LimitRegister = NextProc->LimitReg;
+    TotalPages = NextProc->Total_Pages;
+
+    ACC = NextProc->ACC;
+    PSW[0] = NextProc->PSW[0];
+    PSW[1] = NextProc->PSW[1];
+
+    PC = NextProc->PC;
+}
+
+// This function saves the state of the VM into the PCB of the process that just completed its "time slice"
+void SaveState(struct PCB **PrevProc)
+{
+    (*PrevProc)->P0 = PRegs[0];
+    (*PrevProc)->P1 = PRegs[1];
+    (*PrevProc)->P2 = PRegs[2];
+    (*PrevProc)->P3 = PRegs[3];
+
+    (*PrevProc)->R0 = RRegs[0];
+    (*PrevProc)->R1 = RRegs[1];
+    (*PrevProc)->R2 = RRegs[2];
+    (*PrevProc)->R3 = RRegs[3];
+
+    (*PrevProc)->BaseReg = BaseRegister;
+    (*PrevProc)->LimitReg = LimitRegister;
+    (*PrevProc)->Total_Pages = TotalPages;
+
+    (*PrevProc)->ACC = ACC;
+    (*PrevProc)->PSW[0] = PSW[0];
+    (*PrevProc)->PSW[1] = PSW[1];
+
+    (*PrevProc)->PC = PC;
+}
+
+// Frees the current process
+void DeletePCB(struct PCB *Current)
+{
+    Current->Next_PCB = NULL;
+    free(Current);
+}
+
+// Removes the process at the head of the Ready Queue and returns a pointer to it
+struct PCB *GetNextProcess(struct PCB **Head)
+{
+    struct PCB *tmp;
+    tmp = *Head;
+    if (tmp == NULL)
+    {
+        printf("No more processes to get fool!\n");
+        exit(0);
+    }
+    *Head = (*Head)->Next_PCB;
+    tmp->Next_PCB = NULL;
+    return (tmp);
+}
+
+/*  Takes a pointer to the currently executing process and moves it to the tail of
+    the Ready Queue (RQT)
+*/
+void MvToTail(struct PCB *Current, struct PCB **RQT)
+{
+    (*RQT)->Next_PCB = Current;
+    *RQT = Current;
+    (*RQT)->Next_PCB = NULL;
+    if (RQ == NULL)
+        RQ = *RQT;
+}
+
+// Prints out the Ready Queue
+void PrintQ(struct PCB *Head)
+{
+    struct PCB *tmp;
+    tmp = Head;
+    if (tmp == NULL)
+    {
+        printf("Ready Queue is empty!\n");
+        return;
+    }
+    while (tmp != NULL)
+    {
+        printf("Process ID %d. \n", tmp->PID);
+        tmp = tmp->Next_PCB;
+    }
 }
